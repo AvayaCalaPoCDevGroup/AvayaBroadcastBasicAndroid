bb9aa239099bb2c2b63a6752cdd81c3ea14ba49c
martinez71 <j_alberto_v1@hotmail.com>
2020-03-17 16:23:17 -0600

    Commit Inicial 2020-03-17
    Codigo re acondicionado para conectarse al endpoint de app engine

---
+++ app/build/generated/source/buildConfig/release/com/avaya/android/vantage/basic/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.avaya.android.vantage.basic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.avaya.android.vantage.devcala";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0_2_20200317_1155";
+  // Fields from default config.
+  public static final String AVAYA_BUILD_NUMBER = "0411";
+  public static final String AVAYA_VERSION = "2.0.0";
+  public static final String CSDK_VERSION = "294.0.30";
+  public static final String USER_AGENT_NAME = "Avaya Vantage Basic";
+  public static final java.util.Date buildTime = new java.util.Date(1584467749544L);
+}
---
+++ app/src/main/java/com/avaya/android/vantage/basic/views/KeysNonVersionControl.java
@@ -0,0 +1,5 @@
+package com.avaya.android.vantage.basic.views;
+
+public class KeysNonVersionControl {
+    public static String KEY_API_TRANSLATE = "AIzaSyCi-pY6MzbeFLgAeO0Ts5yvA65hlceCfLE";
+}
